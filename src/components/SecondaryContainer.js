03cde2dcdfdd29cb498707d8e43aa535829824c1
arthdarji352 <arthdarji20@gmail.com>
2023-09-08 18:41:17 +0530

    secondary container with movielist && usePopular movies hook

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,7 +1,27 @@
 import React from "react";
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
-  return <div>SecondaryContainer</div>;
+  const movies = useSelector((store) => store.movies);
+  return (
+    <div className=" bg-black">
+      <div className="-mt-52 pl-12 relative z-20">
+        {" "}
+        <MovieList title={"Now playing "} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Popular"} movies={movies.popularMovies} />
+        <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Horror Movies "} movies={movies.nowPlayingMovies} />
+        {/*     
+    movielist-popular
+    moviecard
+    movielist-trending
+    movielist-nowplaying
+    movielist-horror */}
+      </div>
+    </div>
+  );
 };
 
 export default SecondaryContainer;
